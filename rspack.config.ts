4df35da68ab149c836ac77467545f15ed029f89f
Miyuru Gunarathna <miyurupriyawadan@gmail.com>
2025-04-27 00:04:52 +0530

    feat: initialized project

---
+++ rspack.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "@rspack/cli";
+import rspack from "@rspack/core";
+import ReactRefreshPlugin from "@rspack/plugin-react-refresh";
+
+const isDev = process.env.NODE_ENV === "development";
+const targets = ["chrome >= 87", "edge >= 88", "firefox >= 78", "safari >= 14"];
+
+export default defineConfig({
+  context: __dirname,
+  entry: {
+    main: "./src/main.tsx",
+  },
+  resolve: {
+    extensions: ["...", ".tsx", ".ts", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: "builtin:swc-loader",
+            options: {
+              jsc: {
+                parser: {
+                  syntax: "typescript",
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: "automatic",
+                    development: isDev,
+                    refresh: isDev,
+                  },
+                },
+              },
+              env: { targets },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new rspack.HtmlRspackPlugin({ template: "./index.html" }),
+    isDev && new ReactRefreshPlugin(),
+  ].filter(Boolean),
+});
